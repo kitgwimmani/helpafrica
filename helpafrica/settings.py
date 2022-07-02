20b2c82b381939fb41d12347d0aa78f627414274
mani kitgwim christopher <kitgwimmani@gmail.com>
2022-07-02 17:42:19 +0100

    second wave

--- helpafrica/settings.py
+++ helpafrica/settings.py
@@ -123,7 +123,7 @@ import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 ###############
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #######################
 
 # Default primary key field type
