db055358eb552fdcb5d2a82017d91c1a80be70d7
mani kitgwim christopher <kitgwimmani@gmail.com>
2022-07-02 17:29:25 +0100

    staticfiles

--- helpafrica/settings.py
+++ helpafrica/settings.py
@@ -123,7 +123,7 @@ import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 ###############
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #######################
 
 # Default primary key field type
