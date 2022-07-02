89b0162e528c4b955b76f2493ed1102621ef21e5
mani kitgwim christopher <kitgwimmani@gmail.com>
2022-07-02 17:45:53 +0100

    heroku static

--- helpafrica/settings.py
+++ helpafrica/settings.py
@@ -121,12 +121,20 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-###############
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#######################
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
