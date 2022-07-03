2dc7ff3fd675862ea9b96c021bf5ccbcad446bdc
mani kitgwim christopher <kitgwimmani@gmail.com>
2022-07-03 14:46:11 +0100

    settings

--- helpafrica/urls.py
+++ helpafrica/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('', include('mainapp.urls')),
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATICFILES_DIRS}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
