61e2e5e9d36e391643be1315acc7299c33820bd9
mani kitgwim christopher <kitgwimmani@gmail.com>
2022-07-03 15:45:50 +0100

    settings.Debug

--- helpafrica/urls.py
+++ helpafrica/urls.py
@@ -38,5 +38,7 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
-urlpatterns = urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG == True:
+    urlpatterns += urlpatterns+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
