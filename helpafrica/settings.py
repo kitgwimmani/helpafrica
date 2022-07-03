3f3b6afc36413e54ca180cf4e9617f4d50ae127d
mani kitgwim christopher <kitgwimmani@gmail.com>
2022-07-03 02:07:38 +0100

    static set

--- helpafrica/settings.py
+++ helpafrica/settings.py
@@ -113,13 +113,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-import os
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
